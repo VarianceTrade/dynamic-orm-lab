34a813215e237d9835f5712f973e43d879932e02
Brandon <sugb73@gmail.com>
2021-03-27 12:16:02 -0400

    commit tree ca83149f1bbb30e90621b1ecdf5ab6b36b3985ab

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,11 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    # This is all that belongs here, right?
+    # attr_accessor :id, :name, :grade #???????`
+    
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
+
 end
